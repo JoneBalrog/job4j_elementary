0bd0e58bf45e7afb44f9faf5acd710bce88e776d
JoneBalrog <86967794+JoneBalrog@users.noreply.github.com>
2021-07-06 01:34:19 +0300

    Delete StudentInfo.java

--- src/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j;
-
-public class StudentInfo {
-    public static void main(String[] args) {
-        System.out.println("Big Jone");
-        System.out.println("01.01.1990");
-
-    }
-}
